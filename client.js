1782c41b615bad7299e61326e9f468998c2f3409
Spencer <spencerb242@proton.me>
2022-08-31 16:25:30 -0600

    updated snake client

---
+++ client.js
@@ -0,0 +1,27 @@
+const net = require("net");
+const { setInterval } = require("timers/promises");
+const { IP, PORT } = require("./constants");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: IP,
+    port: PORT
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to Snek game server!");
+    conn.write("Name: SJB");
+  });
+
+  conn.on("data", (data) => {
+    console.log(data);
+  });
+
+  return conn;
+};
+
+module.exports = { connect };
